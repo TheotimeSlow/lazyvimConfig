9b829b73b49b8eefece14484ed788aed4b6df570
Théotime SLOWINSKI <tsl@atolcd.com>
2025-01-09 15:32:55 +0100

    Update

--- plugins/disabled.lua
+++ plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "stevearc/conform.nvim", enabled = false },
+  { "ibhagwan/fzf-lua", enabled = false},
 }
